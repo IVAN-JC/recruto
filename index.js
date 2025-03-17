7aa9caa8e78b5a92c45df930a5bc7e695915cb47
Ivan Okhrimenko <ohrimenkoivan57@gmail.com>
2025-03-17 19:05:17 +0300

    tr

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function listening() {
 }
 // Осуществили запрос с параметрами
 app.get('/:name&:message', (req, res) => {
-    const utf8 = require('utf8');
     res.writeHead(200, { 'Content-Type': 'text/plain; charset=utf-8'});
     res.write('Hello, ' + req.params.name + "! " + req.params.message + "! ");
     res.end();
